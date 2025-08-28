e92f9e8011f962bdb4523cbbac1097e7008e5c12
Naeymar000 <matiasae3@gmail.com>
2025-08-28 20:18:51 -0300

    Update index.js

--- Flor_Primavera/js/index.js
+++ Flor_Primavera/js/index.js
@@ -1,5 +1,6 @@
 const title = document.querySelector('.title')
-const text = 'Feliz inicio de mes Curso 5to 3ra Informática'
+const text = 'Feliz inicio de mes' 
+const text ='Curso 5to 3ra Informática'
 
 // Create container for better responsive layout
 title.style.display = 'flex'
